80d898e346f75855ee8a38268d23b58424f55f39
侯文君 <junerver@qq.com>
2025-05-09 13:13:44 +0800

    chore: 仅同步镜像

---
+++ run_sync_docker.bat
@@ -0,0 +1,25 @@
+@echo off
+:: 设置代码页为 UTF-8（如果需要支持 UTF-8 编码）
+chcp 65001 >nul
+setlocal enabledelayedexpansion
+
+
+:: 检查SSH连接
+echo 检查SSH连接...
+ssh -o BatchMode=yes -o ConnectTimeout=5 root@192.168.189.243 echo "SSH连接成功" >nul 2>&1
+if %errorlevel% neq 0 (
+    echo 无法通过SSH连接到服务器
+    exit /b 1
+)
+
+:: 部署到服务器并清理旧镜像
+echo 开始部署到服务器...
+ssh root@192.168.189.243 -t "cd /opt/auto_assemble && docker-compose down && docker-compose pull && docker-compose up -d && LATEST_ID=$(docker images 192.168.172.110:5000/auto_assemble-webhook:latest --format '{{.ID}}') && docker images 192.168.172.110:5000/auto_assemble-webhook --format '{{.ID}}' | grep -v $LATEST_ID | xargs -r docker rmi -f"
+if %errorlevel% neq 0 (
+    echo 服务器部署失败
+    exit /b 1
+)
+
+echo 部署完成！
+exit /b 0
+
